cd0d6bc12e74e48763d5ebfe750dbcf212ad5fed
Jan Wilmans <jan.wilmans@gmail.com>
2026-05-07 17:00:57 +0200

    move drawing code into class BarStack

--- src/timelens/webclient/collector.js
+++ src/timelens/webclient/collector.js
@@ -5,47 +5,48 @@ const EventType = {
     VALUE: "value"          // has a valid begin_time + value
 };
 
-function makeEvent(name, type, begin_time, end_time, value = 0) {
-    console.log("make: %s, type: %s, b: %s, e: %s ", name, type, begin_time, end_time);
-    return { name, type, begin_time, end_time, value }
+function makeEvent(name, type, begin_time, end_time, groupId, value) {
+    //console.log("make: %s, type: %s, b: %s, e: %s ", name, type, begin_time, end_time);
+    return {
+        name: name,
+        type: type,
+        begin_time: begin_time,
+        end_time: end_time,
+        groupId: groupId,
+        value: value
+    };
 }
 
 class Collector {
     constructor() {
         this.incoming = []; // this an array of structs, if GC becomes a problem, we should turn this into a struct of arrays for zero-reallocation
         this.running = true;
-        this.origin = 0;
 
         // this uses the 'host' where we are loading this application from
-        this.ws = new WebSocket(`ws://${window.location.host}/ws`);
-        this.ws.onmessage = (event) => {
+        //this.ws = new WebSocket(`ws://${window.location.host}/ws`);
 
-            if (this.running === false) {
-                this.origin = 0;
-                return;
-            }
+        this.ws = new WebSocket(`ws://172.16.2.17:8080/ws`);
+        this.ws.onmessage = (event) => {
 
             const data = JSON.parse(event.data);
             // notice that the variables MUST correspond with the actual JSON field names here!
-            const { name, cat, ph, pid, tid, ts } = data;
+            let te = 0;
+            let { name, cat, ph, pid, tid, ts } = data;
 
-            if (this.origin === 0) {
-                this.origin = ts;
-            }
-
-            const relative_ts = (ts - this.origin) / 1000.0; // make times relative and in milliseconds
-
-            const te = relative_ts;
             const value = 0;
             let type = EventType.OPEN;
             if (ph === 'E') {
                 type = EventType.CLOSE;
+                te = ts;
+                ts = 0;
             }
-            this.incoming.push(makeEvent(name, type, relative_ts, te, value));
+            const groupId = tid; // use tid as grouping for single line
+            this.incoming.push(makeEvent(name, type, ts, te, groupId, value));
         }
     }
 
     clear() {
+        console.log("clear");
         // this clears the elements, incoming[0]  // undefined
         // but does not release the underlying storage, so its efficient and GC friendly
         this.incoming.length = 0;
@@ -74,12 +75,12 @@ class Collector {
     }
 
     dummy() {
-        this.incoming.push(makeEvent("cycle", EventType.OPEN, 0));
-        this.incoming.push(makeEvent("capture_image", EventType.DURATION, 10, 230));
+        this.incoming.push(makeEvent("cycle", EventType.OPEN, 0, 0, "groupid"));
+        this.incoming.push(makeEvent("capture_image", EventType.DURATION, 10 * 1000, 230 * 1000, "groupid"));
 
-        this.incoming.push(makeEvent("process_image", EventType.OPEN, 231, 0));
-        this.incoming.push(makeEvent("set_outputs", EventType.DURATION, 310, 400));
-        this.incoming.push(makeEvent("process_image", EventType.CLOSE, 0, 300)); // intentionally out-of-order
-        //this.incoming.push(makeEvent("cycle", EventType.CLOSE, 0, 500)); // intentionally omitted
+        this.incoming.push(makeEvent("process_image", EventType.OPEN, 231 * 1000, 0, "groupid"));
+        this.incoming.push(makeEvent("set_outputs", EventType.DURATION, 310 * 1000, 400 * 1000, "groupid"));
+        this.incoming.push(makeEvent("process_image", EventType.CLOSE, 0, 300 * 1000, "groupid")); // intentionally out-of-order
+        //this.incoming.push(makeEvent("cycle", EventType.CLOSE, 0, 500*1000, "groupid")); // intentionally omitted
     }
 }
--- src/timelens/webclient/scope_controls.js
+++ src/timelens/webclient/scope_controls.js
@@ -80,6 +80,15 @@ function getColor(c) {
     return `hsl(${hue}, 100%, 50%)`;
 }
 
+class Line {
+    constructor(y) {
+        this.y = y;
+        this.openMap = new Map(); // limitation: no support for nested events with the same name
+        this.closedEvents = [];
+        this.lastEndTime = 0;
+    }
+}
+
 class BarStack {
     constructor(ctx, mouseX, mouseY) {
         this.ctx = ctx;
@@ -88,26 +97,108 @@ class BarStack {
         this.color = 1;
         this.y = 20;
         this.height = 12;
+        this.lines = new Map();
+        this.beginTime = Infinity;
     }
 
-    drawEvent(event) {
+    getLine(id) {
+        let entry = this.lines.get(id);
+        if (!entry) {
+            entry = this.makeLine();
+            console.log("new line: " + entry.y);
+            this.lines.set(id, entry);
+        }
+        return entry;
+    }
+
+    makeLine() {
+        const line = new Line(this.y);
+        this.y += 20;
+        return line;
+    }
+
+    drawEvent(line, event) {
         const duration = event.end_time - event.begin_time;
-        this.drawBar(event, duration, event.name);
+        this.drawBar(line, event, event.name);
+    }
+
+    drawEvents() {
+        for (const [, line] of this.lines) { // ignore id
+            const unclosedEvents = Array.from(line.openMap.values());
+            for (const event of unclosedEvents) {
+                event.end_time = line.lastEndTime;      // notice: modifies the event without copying
+                this.drawEvent(line, event);
+            }
+
+            for (const event of line.closedEvents) {
+                this.drawEvent(line, event);
+            }
+        }
+    }
+
+    drawTextOnBar(hover, duration, x, y) {
+        // width label
+        this.ctx.fillStyle = "black";
+        this.ctx.font = "10px monospace";
+        this.ctx.textAlign = "center";
+        this.ctx.textBaseline = "middle";
+        this.ctx.fillText(`${hover} (${duration.toFixed(3)} ms)`, x, y);
+    }
+
+    drawTooltip(hover, duration) {
+        const text = `${hover} (${duration.toFixed(3)} ms)`;
+
+        this.ctx.font = "10px monospace";
+
+        // Measure text size
+        const metrics = this.ctx.measureText(text);
+        const padding = 6;
+
+        const tooltipWidth = metrics.width + padding * 2;
+        const tooltipHeight = 16;
+
+        // Position near mouse
+        const tx = this.mouseX + 12;
+        const ty = this.mouseY - 24;
+
+        // Background
+        this.ctx.fillStyle = "rgba(0, 0, 0, 0.85)";
+        this.ctx.fillRect(tx, ty, tooltipWidth, tooltipHeight);
+
+        // Border
+        this.ctx.strokeStyle = "#00ff88";
+        this.ctx.strokeRect(tx, ty, tooltipWidth, tooltipHeight);
+
+        // Text
+        this.ctx.fillStyle = "#00ff88";
+        this.ctx.textAlign = "left";
+        this.ctx.textBaseline = "middle";
+
+        this.ctx.fillText(
+            text,
+            tx + padding,
+            ty + tooltipHeight / 2
+        );
     }
 
     // show the text by default, but show 'hover' if the mouse is over the bar
-    drawBar(event, text, hover) {
-
+    drawBar(line, event, hover) {
         assert(typeof event.name === "string", "event.name must be string");
 
         const color = getColor(this.color);
         this.color += 1;
 
+        const bt = (event.begin_time - this.beginTime) / 1000;
+        const et = (event.end_time - this.beginTime) / 1000;
+        const durationMs = et - bt;
+
         const scale = 2;
-        const x1 = Math.round(event.begin_time * scale);
-        const x2 = Math.round(event.end_time * scale);
+        const x1 = Math.round(bt * scale);
+        const x2 = Math.round(et * scale);
+
+        //console.log("draw: y: " + line.y + " x1: " + x1 + " x2: " + x2);
         const width = x2 - x1;
-        const isHovered = this.mouseX >= x1 && this.mouseX <= x2 && this.mouseY >= this.y && this.mouseY <= this.y + this.height;
+        const isHovered = this.mouseX >= x1 && this.mouseX <= x2 && this.mouseY >= line.y && this.mouseY <= line.y + this.height;
 
         if (event.type === EventType.OPEN) {
             const gradient = this.ctx.createLinearGradient(x1, 0, x2, 0);
@@ -116,23 +207,17 @@ class BarStack {
             gradient.addColorStop(1, "rgba(0,0,0,0)");
 
             this.ctx.fillStyle = gradient;
-            this.ctx.fillRect(x1, this.y, width, this.height);
+            this.ctx.fillRect(x1, line.y, width, this.height);
         }
         else {
             this.ctx.fillStyle = color;
-            this.ctx.fillRect(x1, this.y, width, this.height);
+            this.ctx.fillRect(x1, line.y, width, this.height);
         }
 
         if (isHovered) {
-            // width label
-            this.ctx.fillStyle = "black";
-            this.ctx.font = "10px monospace";
-            this.ctx.textAlign = "center";
-            this.ctx.textBaseline = "middle";
-            this.ctx.fillText(String(hover + " (" + text + "ms)"), x1 + width / 2, this.y + this.height / 2);
+            //this.drawTextOnBar(hover, durationMs, x1 + width / 2, line.y + this.height / 2);
+            this.drawTooltip(hover, durationMs);
         }
-
-        this.y += this.height;
     }
 }
 
@@ -188,53 +273,41 @@ class Scope {
         const ctx = this.canvas.getContext("2d");
         ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        const bars = new BarStack(ctx, this.mouseX, this.mouseY);
         const data = this.collector.data();
 
-        // active open events (by name or id)
-        const openMap = new Map(); // limitation: no support for nested events with the same name
-        const closedEvents = [];
-
-        let lastTime = 0;
         for (const event of data) {
-
-            if (event.end_time > lastTime) {
-                lastTime = event.end_time;
+            const line = bars.getLine(event.groupId);
+            if (event.end_time > line.lastEndTime) {
+                line.lastEndTime = event.end_time;
             }
 
             if (event.type === EventType.OPEN) {
-                openMap.set(event.name, event);
+                line.openMap.set(event.name, event);
+
+                if (event.begin_time < bars.beginTime) {
+                    bars.beginTime = event.begin_time;
+                }
             }
 
             if (event.type === EventType.CLOSE) {
-                const entry = openMap.get(event.name);
+                const entry = line.openMap.get(event.name);
                 if (!entry) continue;
                 const open = {
                     ...entry,   // take a copy
                     end_time: event.end_time,
                     type: EventType.CLOSE
                 };
-                closedEvents.push(open);
-                openMap.delete(event.name);
+                line.closedEvents.push(open);
+                line.openMap.delete(event.name);
             }
-        }
 
-        const bars = new BarStack(ctx, this.mouseX, this.mouseY);
-
-        const unclosedEvents = Array.from(openMap.values());
-        for (const event of unclosedEvents) {
-            event.end_time = lastTime;      // notice: modifies the event without copying
-            bars.drawEvent(event);
-        }
-
-        for (const event of closedEvents) {
-            bars.drawEvent(event);
-        }
-
-        for (const event of data) {
             if (event.type === EventType.DURATION) {
-                bars.drawEvent(event);
+                line.closedEvents.push(event);
             }
         }
+
+        bars.drawEvents();
     }
 
     resize(width, height) {
