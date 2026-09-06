60988a7a7fa3c834465870999e5c32d87ea4609c
Jan Wilmans <jan.wilmans@gmail.com>
2026-09-06 15:06:02 +0200

    update start_fastapi.sh for debian without pip installed

--- start_fastapi.sh
+++ start_fastapi.sh
@@ -7,19 +7,25 @@ cd "$SCRIPTDIR"
 
 echo "Working directory: $PWD"
 
+if ! python3 -m venv --help >/dev/null 2>&1; then
+    echo "ERROR: Python venv support is not installed."
+    echo "Please install it with:"
+    echo "    sudo apt install python3-venv python3-pip"
+    exit 1
+fi
+
 # Create venv if it doesn't exist
 if [ ! -f "venv/bin/activate" ]; then
     echo "Creating virtual environment..."
     rm -rf venv
     python3 -m venv venv
-    python -m pip install --upgrade pip
-    python -m pip install .
-    python -m pip install -e ".[dev]"
+    venv/bin/python -m pip install --upgrade pip
+    venv/bin/python -m pip install -e ".[dev]"
 fi
 
 source venv/bin/activate
 cd "$SCRIPTDIR/src/timelens"
 echo "Moved to TimeLens directory: $PWD"
 
-python --version
+python3 --version
 exec uvicorn server:app --reload --host 0.0.0.0 --port 8080
